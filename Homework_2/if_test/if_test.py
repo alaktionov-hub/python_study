5ff379ef46dbf954678fc25005c005cb033acdce
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-04 20:34:14 +0200

    test

---
+++ Homework_2/if_test/if_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("For registration, indicate your age?")
+user_age = int(input("How old are you? :"))
+
+if (user_age < 18):
+    print("Are you sure your mom knows where you are?")
+elif (user_age < 30):
+    print("Hello new young user")
+elif (user_age < 60):
+    print("Hello new user")
+else:
+    print("Welcome")
