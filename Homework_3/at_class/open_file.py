209c2e6a5b30d47f94bfd8a705ab5f267d50b2d9
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-06 21:57:40 +0200

    add open_file.py

---
+++ Homework_3/at_class/open_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+filename = sys.argv[1]
+
+# дlets open file (option 'r')
+f = open(filename, 'r')
+for line in f:  # read each line from file
+    print(line)  # Print this line
+f.close()  # and allways better close file
+
+print("Be happy!!!")
