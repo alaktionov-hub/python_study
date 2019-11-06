9cce029a570367279b64e5ddcd81bdfbbf95cbdc
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-06 20:38:16 +0200

    add 1 work at class

---
+++ Homework_3/at_class/for_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Print all Student
+students = ['Vasya', 'Kolay', 'Petya', 'Ivan', 'Dimka']
+
+
+for i in students:
+    print("I'am student.", "My Name is", "{}".format(i))
+
+print("So all Done")
