aa1c417862c5e4dacc49424b067cea5923f5fc7e
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-06 20:57:28 +0200

    add fix and text

--- Homework_3/at_class/random_student.py
+++ Homework_3/at_class/random_student.py
@@ -10,3 +10,13 @@ for student in students:
     for j in range(1, 6):
         stud_dict[student].append(random.randint(1, 5))
 print(stud_dict)
+
+# Part 2 calculate
+# for name in stud_dict:
+#    print(sum(stud_dict.get(name))/len(stud_dict.get(name)))
+#
+# Without text
+
+for name in stud_dict:
+    print("Student {name} has aeverge mark {mark}".format(
+        name=name, mark=sum(stud_dict.get(name))/len(stud_dict.get(name))))
