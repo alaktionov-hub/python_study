6001731684877aba792f30e2f2062614489691d4
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-08 00:01:38 +0200

    fix typo

--- Homework_3/task2/just_play_with_generate.py
+++ Homework_3/task2/just_play_with_generate.py
@@ -6,7 +6,7 @@ filename = sys.argv[1]  # What File we need use. Argument 1
 where_write_file = open(filename, "w")
 
 
-data_fizz_buzz_to_frite = str(np.random.randint(1, 99, size=(20, 3)))
+data_fizz_buzz_to_write = str(np.random.randint(1, 99, size=(20, 3)))
 
-print(data_fizz_buzz_to_frite)
-where_write_file.write(data_fizz_buzz_to_frite)
+print(data_fizz_buzz_to_write)
+where_write_file.write(data_fizz_buzz_to_write)
