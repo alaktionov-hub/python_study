8e1a2c0a13038c9c587203e596f624b7f8832de3
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-07 09:09:01 +0200

    small fix

--- Homework_3/at_class/random_student.py
+++ Homework_3/at_class/random_student.py
@@ -10,6 +10,9 @@ for student in students:
     for j in range(1, 6):
         stud_dict[student].append(random.randint(1, 5))
 print(stud_dict)
+# Example
+#{'Vasya': [3, 2, 2, 2, 2], 'Kolay': [3, 4, 3, 4, 3], 'Petya': [2, 2, 4, 2, 3], 'Ivan': [5, 4, 1, 2, 5], 'Dimka': [1, 2, 2, 2, 5]}
+
 
 # Part 2 calculate
 # for name in stud_dict:
@@ -20,5 +23,12 @@ print(stud_dict)
 for name in stud_dict:
     print("Student {name} has aeverge mark {mark}".format(
         name=name, mark=sum(stud_dict.get(name))/len(stud_dict.get(name))))
+# Example
+# Student Vasya has aeverge mark 2.2
+# Student Kolay has aeverge mark 3.4
+# Student Petya has aeverge mark 2.6
+# Student Ivan has aeverge mark 3.4
+# Student Dimka has aeverge mark 2.4
+
 
 print("All done")
