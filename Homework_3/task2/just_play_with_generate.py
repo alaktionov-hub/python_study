c66ed4eb1377a708ce2e9c45d889eed2f5507064
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-12 17:39:57 +0200

    add new staff

--- Homework_3/task2/just_play_with_generate.py
+++ Homework_3/task2/just_play_with_generate.py
@@ -8,3 +8,4 @@ where_write_file = open(filename, "w")
 np.savetxt(where_write_file, np.random.randint(1, 99, size=(20, 3)), fmt="%s")
 
 # np.savetxt - use to save
+
--- Homework_4/fixed_fizz-buzz.py/fizz-buzz.py
+++ Homework_4/fixed_fizz-buzz.py/fizz-buzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# Fixed version before its write via stdout
+
 import random  # Random module help us generate Fizz Buzz random in range
 import sys  # We need sis module to be able read from file
-import numpy as np  # need for generate file and write us a text
+
 
 filename = sys.argv[1]  # What File we need use. Argument 1
 filename2 = sys.argv[2]  # What File we need use. Argument 1
@@ -10,29 +10,29 @@ filename2 = sys.argv[2]  # What File we need use. Argument 1
 file_with_fuzz_buzz = open(filename, 'w')
 where_keep_answer = open(filename2, 'w')
 
-np.savetxt(file_with_fuzz_buzz, np.random.randint(
-    1, 20, size=(20, 3)), fmt="%s")
 
-
-# np.random will generat efor us all what we need
-
-# Now function return line . Need for fix file write
+for line in range(1, 20+1):
+    # a = str(option1), str(option2), str(option3)
+    # file_with_fuzz_buzz.write(a)
+    file_with_fuzz_buzz.write("{var1} {var2} {var3} \n".format(
+        var1=random.randint(1, 5), var2=random.randint(1, 6), var3=random.randint(1, 20)))
 
 
 def fizz_buzz(fizz, buzz, third_number):
     i = 1
-    line = ""  # Create variables lin
+    line = ""
     while i < (third_number+1):
         if i % fizz == 0 and i % buzz == 0:
-            line += "FB "  # add to line
+            line += "FB "
         elif i % fizz == 0:
-            line += "F "  # add to line
+            line += "F "
         elif i % buzz == 0:
-            line += "B "  # add to line
+            line += "B "
         else:
-            line += str(i) + " "  # add to line
+            line += str(i) + " "
         i += 1
-    return line  # Function return line
+    return line
+    print(line)
 
 
 file_with_fuzz_buzz = open(filename, 'r')
@@ -41,9 +41,10 @@ file_with_fuzz_buzz = open(filename, 'r')
 file_with_fuzz_buzz = open(filename, 'r')
 for line in file_with_fuzz_buzz:  # read each line from file
     li = line.split()
+    li = list(map(int, li))  # Map all from line
     # Pass each number to our function and convert it to int
     # fizz_buzz(int(li[0]), int(li[1]), int(li[2]))
-    where_keep_answer.write(
-        fizz_buzz(int(li[0]), int(li[1]), int(li[2]))+"\n")  # Call function with new lin
-file_with_fuzz_buzz.close()  # close files
-file_with_fuzz_buzz.close()  # close files
+#    where_keep_answer.write(fizz_buzz(int(li[0]), int(li[1]), int(li[2])))
+    where_keep_answer.write(fizz_buzz(int(li[0]), int(li[1]), int(li[2]))+"\n")
+
+file_with_fuzz_buzz.close()
