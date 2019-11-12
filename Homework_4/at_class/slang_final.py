4245e9ad2fa2c6bf826d7b6323cf88d5e435d0d5
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-12 14:22:15 +0200

    add fixed python

--- Homework_4/at_class/slang_final.py
+++ Homework_4/at_class/slang_final.py
@@ -15,7 +15,7 @@ slang_phrases = [", yeah!", ", this is crazy, I tell ya.", ", can U believe this
                  ", eh?", ", aw yea.", ", yo.", "? No way!", ". Awesome!"]
 
 splited_string = re.findall(r'[^.!?]+', line_out)
-
+print(splited_string)
 f_text = open(sys.argv[2], 'w')
 f_text.write((" ").join([str(x)+random.choice(slang_phrases)
                          for x in splited_string]))
---
+++ Homework_4/fixed_fizz-buzz.py/fizz-buzz.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import random  # Random module help us generate Fizz Buzz random in range
+import sys  # We need sis module to be able read from file
+
+
+filename = sys.argv[1]  # What File we need use. Argument 1
+filename2 = sys.argv[2]  # What File we need use. Argument 1
+
+file_with_fuzz_buzz = open(filename, 'w')
+where_keep_answer = open(filename2, 'w')
+
+
+for line in range(1, 20+1):
+    # a = str(option1), str(option2), str(option3)
+    # file_with_fuzz_buzz.write(a)
+    file_with_fuzz_buzz.write("{var1} {var2} {var3} \n".format(
+        var1=random.randint(1, 5), var2=random.randint(1, 6), var3=random.randint(1, 20)))
+
+
+def fizz_buzz(fizz, buzz, third_number):
+    i = 1
+    line = ""
+    while i < (third_number+1):
+        if i % fizz == 0 and i % buzz == 0:
+            line += "FB "
+        elif i % fizz == 0:
+            line += "F "
+        elif i % buzz == 0:
+            line += "B "
+        else:
+            line += str(i) + " "
+        i += 1
+    return line
+    print(line)
+
+
+file_with_fuzz_buzz = open(filename, 'r')
+
+# lets open file (option 'r')
+file_with_fuzz_buzz = open(filename, 'r')
+for line in file_with_fuzz_buzz:  # read each line from file
+    li = line.split()
+    li = list(map(int, li))  # Map all from line
+    # Pass each number to our function and convert it to int
+    # fizz_buzz(int(li[0]), int(li[1]), int(li[2]))
+    where_keep_answer.write(fizz_buzz(int(li[0]), int(li[1]), int(li[2])))
+    # print()
+
+file_with_fuzz_buzz.close()
