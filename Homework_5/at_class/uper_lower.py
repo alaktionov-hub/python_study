aa253b252a393744fa73a41a0d27a3bcdb1f2582
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-14 14:08:18 +0200

    fix task 2

--- Homework_5/at_class/uper_lower.py
+++ Homework_5/at_class/uper_lower.py
@@ -2,21 +2,33 @@
 upper_sample = str(input("Please give me lower line: "))
 lower_sample = str(input("Please give me Upper line: "))
 
+lines = str(input("Please give me Upper and Lower list: "))
+
 # Function for upper
 
 
 def upper_fun(line):
     new_line = line.upper()  # Just use upper
-    print(new_line)
+    return new_line
 
 # Function for lower
 
 
 def lower_fun(line):
     new_line = line.lower()  # Just use lower
-    print(new_line)
+    return new_line
 
 
 # call function what will print result
-upper_fun(upper_sample)
-lower_fun(lower_sample)
+print(upper_fun(upper_sample))
+print(lower_fun(lower_sample))
+
+
+lines_sp = lines.split()
+
+map_line = list(map(upper_fun, lines_sp))
+map_line2 = list(map(lower_fun, lines_sp))
+
+
+print(map_line)
+print(map_line2)
