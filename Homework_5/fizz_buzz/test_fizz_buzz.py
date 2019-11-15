95ab8a2a6c9d2102170d4e0edb417adcc705a206
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-11-15 21:17:17 +0200

    just play

---
+++ Homework_5/fizz_buzz/test_fizz_buzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+#  NOT HOMEWORK
+#  Just try to play with some examples from internet
+#
+
+fizz = int(input("Please input fizz value: "))
+buzz = int(input("Please input buzz value: "))
+third_number = int(input("Please input third_number value: "))
+
+
+print("\n".join(["F"*(i % fizz == 0)+"B"*(i % buzz == 0) or str(i)
+                 for i in range(1, third_number+1)]))
