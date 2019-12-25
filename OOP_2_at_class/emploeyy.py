a8bc1eea35542dbd20b6fc7a0ce62e71765eb85a
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-12-26 00:18:28 +0200

    update task

--- OOP_2_at_class/emploeyy.py
+++ OOP_2_at_class/emploeyy.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+import datetime
 
+
+now = datetime.date.today()
+start_month = datetime.date(now.year, now.month, 1)
+diff = (now - start_month).days + 1
+# Calculate work day in month Task 8
+weekend = [5, 6]
+holidays = [datetime.date(now.year, 12, 25), datetime.date(now.year, 10, 14),
+            datetime.date(now.year, 8, 24), datetime.date(now.year, 6, 28), datetime.date(now.year, 5, 1)]
+
+working_days = 0
+for day in range(diff):
+    if (start_month + datetime.timedelta(day)).weekday() not in weekend and start_month + datetime.timedelta(day) not in holidays:
+        working_days += 1
 # TASK 1
 # Create class with emmloyee . And save there all data about worker . Name , mail , phone , and salary for 1 day .
 # NOT GOOD DAY ! NOT GOOD DAY ! NOT GOOD DAY ! NOT GOOD DAY
@@ -7,17 +21,21 @@
 
 
 class Employee:
-    def __init__(self, name, email, phone, day_salary):
+    def __init__(self, name, email, phone, day_salary, **data):  # data = for task 9
         self.name = name
         self.email = email
         self.phone = phone
         self.day_salary = day_salary
+        self.data = data
 
     def work(self):
         return("So I am here .")
 # ADD Class what will calculate salary for some days Calculate how mutch person get
 
     def check_salary(self, days):
+        return self.day_salary*days
+
+    def check_salary2(self, days=working_days):
         return self.day_salary*days
 
 # Create class what inhavied from Employee
@@ -51,13 +69,24 @@ class Programmer(Employee):
 
 print("Task print info for Recruter" + '\n')
 
-recruterHR = Recruter("Vasya", "HR@hell.com", "0668746111", 66.6)
+recruterHR = Recruter("Vasya", "HR@hell.com",
+                      "0668746111", 66.6, hired_this_month=10, info_about="I do not love this")
 print(recruterHR.work())
 print(recruterHR.__str__())
 print(recruterHR.check_salary(666))  # Salary at hell
+print('\n' + "test task 8" + '\n')
+print(recruterHR.check_salary2())  # Salary at hell
+#print('\n' + "test task 9" + '\n')
+#recruterHR.hired_this_month = 13
+# print(recruterHR.hired_this_month)
+#print(recruterHR.hired_this_month + 1)
+#print(recruterHR.hired_this_month - 3)
+# print('\n' + "test task 9" + '\n')
+print('\n' + "test task 9" + '\n')
+print(recruterHR.data)
+print('\n' + "end task 9" + '\n')
 
-
-print("Task print info for Recruter" + '\n')
+print("Task print info for Recruter2" + '\n')
 
 recruterHR2 = Recruter("Vasya", "HR@hell.com", "0668746111", 666.6)
 print(recruterHR2.work())
@@ -67,11 +96,19 @@ print(recruterHR2.check_salary(666))  # Salary at hell
 print('\n' + "Task print info for Programmer" + '\n')
 
 programmer_Ivan = Programmer(
-    "Ivan", "to_ivan@ne_tuda.com", "+41212234243", 133)
+    "Ivan", "to_ivan@ne_tuda.com", "+41212234243", 133, tech_stack=['c++', 'C', 'python'])
 print(programmer_Ivan.work())
 print(programmer_Ivan.__str__())
 print(programmer_Ivan.check_salary(6))
+print('\n' + "test task 8" + '\n')
+print(programmer_Ivan.check_salary2())  # Salary at hell
 
+print('\n' + "test task 10" + '\n')
+print(programmer_Ivan.data.get('tech_stack'))
+print('\n' + "end task 10" + '\n')
+
+
+# Compare Salary
 print('\n' + "Task With Salary Compare" + '\n')
 if recruterHR < recruterHR2:
     print("More big")
