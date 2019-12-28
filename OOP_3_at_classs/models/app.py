d7d36efe6d5234a50de75018750c815d33c36c54
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2019-12-28 13:39:23 +0200

    fix no need this file

--- OOP_3_at_classs/models/app.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-from .programmer import Programmer
-from .recruter import Recruter
--- OOP_3_at_classs/models/programmer.py
+++ OOP_3_at_classs/models/programmer.py
@@ -10,8 +10,8 @@ class Programmer(Employee):
         return "Hello, my name is " + self.name + " " + "And my mail " + self.email + "And you can call me " + self.phone + " and I work here on position Programmer"
 # Need find way to fix for compare also salary not only tech stack
 
-    def __lt__(self, other):
-        return self.day_salary < other.day_salary
+ #   def __lt__(self, other):
+ #       return self.day_salary < other.day_salary
 
     def __gt__(self, other):
         return len(self.data.get("tech_stack")) > len(other.data.get("tech_stack"))
