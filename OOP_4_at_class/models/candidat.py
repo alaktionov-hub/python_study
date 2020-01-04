a2044f6077ef068a3c5d6935b17534e42556f0b2
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-04 13:32:37 +0200

    add 1 more task

--- OOP_4_at_class/models/candidat.py
+++ OOP_4_at_class/models/candidat.py
@@ -9,3 +9,19 @@ class Candidat:
         self.technologies = technologies
         self.main_skill = main_skill
         self.data = data
+
+    def validate(self, mail):
+        print(self.get_emails_from_file())
+        if mail in self.get_emails_from_file():
+            raise ValueError
+
+    def save_email_to_file(self):
+        with open('emails', 'a') as file:
+            file.write(self.email)
+            file.write('\n')
+
+    def get_emails_from_file(self):
+        with open('emails', 'a+') as file:
+            file.seek(0)
+            data = file.read()
+            return data.split('\n')
