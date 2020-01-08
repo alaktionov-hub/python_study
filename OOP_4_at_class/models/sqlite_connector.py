7ea53be0840fd6362ea130d03a89084876f9cded
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-08 10:43:01 +0200

    remove no needed part

--- OOP_4_at_class/models/sqlite_connector.py
+++ OOP_4_at_class/models/sqlite_connector.py
@@ -10,41 +10,6 @@ class Sqlite_connector:
         # name of the table in the properties of class
         self.table_name = table_name
 
-    def create_table(self, db_name):
-        # create table for 5 types of objects
-        if self.table_name in ['programmers', 'recruiters', 'candidates', 'vacancies', 'iterviews']:
-            # only tables with names same to classes names can be created
-            fields = ''
-            if self.table_name == 'programmers':
-                # form squlite query for table creation for different classes
-                fields = """CREATE TABLE IF NOT EXISTS programmers
-                    (id serial, first_name text, last_name text, email text, 
-                    phone text, hired_at text, salary_per_day integer, main_skill text,
-                    additional_info text)"""
-            elif self.table_name == 'recruiters':
-                fields = """CREATE TABLE IF NOT EXISTS recruiters
-                    (id serial, first_name text, last_name text, email text, 
-                    phone text, hired_at text, salary_per_day integer)"""
-            elif self.table_name == 'candidates':
-                fields = """CREATE TABLE IF NOT EXISTS candidates
-                    (id serial, first_name text, last_name text, email text, 
-                    phone text, main_skill text)"""
-            elif self.table_name == 'vacancies':
-                fields = """CREATE TABLE IF NOT EXISTS vacancies
-                    (id serial, title text, salary_per_day integer, main_skills text, 
-                    technoloies text, recruiter text, hired_at text, status_of_vacncy boolean)"""
-            elif self.table_name == 'iterviews':
-                fields = """CREATE TABLE IF NOT EXISTS iterviews
-                (id serial, vacancy text, programer text, recruiter text, 
-                candidate text, datetime_of_int text, feedback text, result text)"""
-            conn = sqlite3.connect(db_name)
-            cursor = conn.cursor()
-            cursor.execute(fields)
-            conn.close()
-        else:
-            print("Table with name {table} cannot be created!".format(
-                table=self.table_name))
-
     def add_to_db(self, obj, db_name):
         # add rows to db
         conn = sqlite3.connect(db_name)
