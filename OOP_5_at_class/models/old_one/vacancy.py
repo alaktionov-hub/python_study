2fbd9856ff6fd5d210e20f214d253b2e91c0e628
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-08 19:55:06 +0200

    clean

--- OOP_5_at_class/models/old_one/vacancy.py
+++
@@ -1,6 +0,0 @@
-class Vacancy:
-    def __init__(self, title, main_skill, main_skills_level, **data):
-        self.title = title
-        self.main_skill = main_skill
-        self.main_skills_level = main_skills_level
-        self.data = data
