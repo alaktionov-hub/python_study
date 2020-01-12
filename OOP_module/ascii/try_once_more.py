e98733cc3f0648689cd5a9f3c34921352f7d40bc
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-13 00:10:31 +0200

    save some result

---
+++ OOP_module/ascii/try_once_more.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+ASCII scene for try once more .
+"""
+print(r"""
+
+                           _
+                          ( )
+                           H
+                           H
+                          _H_
+                       .-'-.-'-.
+                      /         \
+                     |           |
+                     |   .-------'._
+                     |  / /  '.' '. \
+                     |  \ \ @   @ / /
+                     |   '---------'
+                     |    _______|
+                     |  .'-+-+-+|
+                     |  '.-+-+-+|         Higher nose Leather bag !!!!!!!
+                     |    """""" |           You can Always try once more ;)
+                     '-.__ _ __.-'      see you loser!
+                                                        """)
