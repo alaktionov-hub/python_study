875284866034b2d0d34849d1eb298d009803f778
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-22 20:34:23 +0200

    add new staff

--- html_2/main.js
+++ html_2/main.js
@@ -1,3 +1,13 @@
 let console1 = document.getElementById('console');
 
 console.log(console1)
+
+console1.innerHTML = 'lorem';
+
+let form = document.getElementById('form1');
+
+console.log(form);
+
+form.addEventListener('submit', function () {
+
+});
