b004aa5229cad50c6cd357a2c665eb94cfcfb54b
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-01-25 13:53:34 +0200

    add new task

--- html_2/main.js
+++ html_2/main.js
@@ -1,13 +1,23 @@
-let console1 = document.getElementById('console');
+//let console1 = document.getElementById('console');
+//
+//console.log(console1)
+//
+//console1.innerHTML = 'lorem';
+//
+//let form = document.getElementById('form1');
+//
+//
+//let emailField = document.getElementById('exampleInputEmail1');
+//let passwordField = document.getElementById('exampleInputPassword');
+//
+//
+//console.log(form);
+//
+//form.addEventListener('submit', function (event) {
+//	event.preventDefaul();
+//	console.log(emailField.nodeValue, passwordField.nodeValue);
+//	console1.innerHTML = `${emailField} ${passwordField}`;
+//});
+let $counter = document.getElementById('counter');
 
-console.log(console1)
-
-console1.innerHTML = 'lorem';
-
-let form = document.getElementById('form1');
-
-console.log(form);
-
-form.addEventListener('submit', function () {
-
-});
+$counter.innerHTML = 2
