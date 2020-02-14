8fd4124e0781d79d3758607dd3f57aefba4ba124
Anatoliy Laktionov <anatoliy.laktionov@gameloft.com>
2020-02-14 23:40:46 +0200

    add small task

--- html6/main.js
+++ html6/main.js
@@ -1,97 +1,140 @@
 let $field = document.getElementById('field');
 let ctx = $field.getContext('2d');
 
-let dir = [10, 0];
+class Point {
 
-let dir = new Point(10, 0)
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
 
-let body = [
+	isEqual(another) {
+		return this.x === another.x && this.y === another.y;
+	}
+
+	add(another) {
+		return new Point(this.x + another.x, this.y + another.y);
+	}
+
+	static getRandom() {
+		return new Point(Math.ceil(Math.random() * 49) * 10, Math.ceil(Math.random() * 49) * 10);
+	}
+
+	isOpposite(another) {
+		return (this.x + another.x === 0) && (this.y + another.y === 0);
+	}
+}
+
+class Snake {
+	constructor(body) {
+		this.body = body;
+	}
+
+	getHead() {
+		return this.body[0];
+	}
+
+	add(point) {
+		this.body.unshift(snake.getHead().add(point));
+	}
+
+	move(point) {
+		let head = this.getHead().add(dir);
+		if (head.x < 0) {
+			head.x = 490;
+		}
+		if (head.x > 490) {
+			head.x = 0;
+		}
+		if (head.y < 0) {
+			head.y = 490;
+		}
+		if (head.y > 490) {
+			head.y = 0;
+		}
+		this.body.unshift(head);
+		this.body.pop();
+	}
+}
+
+class Direction extends Point {
+	move(another) {
+		if (!this.isOpposite(another)) {
+			this.x = another.x;
+			this.y = another.y;
+		}
+	}
+}
+
+let dir = new Direction(10, 0);
+
+let snake = new Snake([
 	new Point(30, 10),
 	new Point(20, 10),
-	new Pontt(10, 10),
-];
+	new Point(10, 10),
+]);
 
-let cherry = [-10, -10];
-let food = [100, 100];
+let cherry = new Point(-10, -10);
+let food = new Point(100, 100);
 
 
-setInterval(function () {
+let loopId = setInterval(function () {
 	ctx.clearRect(0, 0, 500, 500);
 
-	if (body[0][0] === cherry[0] && body[0][1] === cherry[1]) {
-		cherry = [-10, -10];
-		body.unshift(move(body[0], dir));
-		body.unshift(move(body[0], dir));
-	} else if (body[0][0] === food[0] && body[0][1] === food[1]) {
-		food = [Math.ceil(Math.random() * 50) * 10, Math.ceil(Math.random() * 50) * 10];
-		body.unshift(move(body[0], dir));
+	if (snake.getHead().isEqual(cherry)) {
+		cherry = new Point(-10, -10);
+		snake.add(dir);
+		snake.add(dir);
+	} else if (snake.getHead().isEqual(food)) {
+		food = Point.getRandom();
+		snake.add(dir);
 	} else {
-
-		let head = move(body[0], dir);
-
-		if (head[0] < 0) {
-			head[0] = 490;
-		}
-
-		if (head[0] > 490) {
-			head[0] = 0;
-		}
-
-		if (head[1] < 0) {
-			head[1] = 490;
-		}
-
-		if (head[1] > 490) {
-			head[1] = 0;
-		}
-
-		body.unshift(head);
-		body.pop();
+		snake.move(dir);
 	}
 
 	ctx.fillStyle = 'red';
-	ctx.fillRect(cherry[0], cherry[1], 10, 10);
+	ctx.fillRect(cherry.x, cherry.y, 10, 10);
 
 	ctx.fillStyle = 'green';
-	ctx.fillRect(food[0], food[1], 10, 10);
+	ctx.fillRect(food.x, food.y, 10, 10);
 
 	ctx.fillStyle = 'black';
-	for (let block of body) {
-		ctx.fillRect(block[0], block[1], 10, 10);
+	for (let block of snake.body) {
+		ctx.fillRect(block.x, block.y, 10, 10);
 	}
-	ctx.fillText('Accountant Oksana', body[0][0] + 10, body[0][1] - 10)
+
+	for (let element of snake.body.slice(1)) {
+		if (element.isEqual(snake.getHead())) {
+			alert('Game over');
+			clearInterval(loopId);
+		}
+	}
+
+	ctx.fillText('dfgfdgdf', snake.getHead().x + 10, snake.getHead().y - 10);
+
 
 }, 100);
 
 
 setInterval(function () {
-	cherry = [Math.ceil(Math.random() * 50) * 10, Math.ceil(Math.random() * 50) * 10]
+	cherry = Point.getRandom();
 }, 5000);
 
 
-function canChangeDir(dir1, dir2) {
-	console.log(dir1, dir2);
-	return dir1[0] + dir2[0] !== 0 || dir1[1] + dir2[1] !== 0;
-}
-
-
 document.addEventListener('keydown', function (event) {
+
 	switch (event.key) {
 		case 'ArrowRight':
-			if (canChangeDir(dir, [10, 0]))
-				dir = [10, 0];
+			dir.move(new Point(10, 0));
 			break;
 		case 'ArrowLeft':
-			if (canChangeDir(dir, [-10, 0]))
-				dir = [-10, 0];
+			dir.move(new Point(-10, 0));
 			break;
 		case 'ArrowUp':
-			if (canChangeDir(dir, [0, -10]))
-				dir = [0, -10];
+			dir.move(new Point(0, -10));
 			break;
 		case 'ArrowDown':
-			if (canChangeDir(dir, [0, 10]))
-				dir = [0, 10];
+			dir.move(new Point(0, 10));
 			break;
 	}
 });
